788aa813110886381e8245e5575ab130eb8f75d2
Umar <umar_14@msn.com>
2019-08-08 10:39:25 +0500

    Create script_name.js

---
+++ script_name.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         FiverrBuyers
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.fiverr.com/users/<YOUR_FIVERR_USERNAME>/requests
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // your code here
+        console.log('Done Loading.');
+        var i = setInterval(function() {
+
+            if (document.getElementsByClassName('user-pict-40')[0])
+            {
+                var list = document.getElementsByClassName("btn-standard btn-green-grad js-send-offer");
+                for (let item of list) {
+                    item.text = JSON.parse(item.getAttribute("data-meta"))['username'];
+                }
+                console.log('FiverrBuyers-Success');
+
+                document.getElementsByClassName("db-load-more")[0].addEventListener("click", function(){
+
+                    var j = setInterval(function() {
+                        var list = document.getElementsByClassName("btn-standard btn-green-grad js-send-offer");
+                        //console.log(list.length);
+                        for (let item of list) {
+                            item.text = JSON.parse(item.getAttribute("data-meta"))['username'];;
+                        }
+                        console.log("FiverrBuyers-Done");
+                        if (list.length > 35) { clearInterval(j); }
+                        else { console.log("CarryON"); }
+                    },1000)
+
+
+                });
+
+                clearInterval(i);
+            }
+            else
+            {
+                console.log('0');
+            }
+
+        }, 1000);
+    }, false);
+
+
+}
+)();
