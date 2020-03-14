dd2bcaa23f4df9078803a3389bb8ffb8b72105d8
Umar <umar_14@msn.com>
2020-03-14 13:13:05 +0500

    Update script_name.js

--- script_name.js
+++ script_name.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.fiverr.com/users/<YOUR_FIVERR_USERNAME>/requests
+// @match        https://www.fiverr.com/users/<YOUR_FIVERR_USERNAME>/*
 // @grant        none
 // ==/UserScript==
 
@@ -23,7 +23,25 @@
                 }
                 console.log('FiverrBuyers-Success');
 
+
+
                 document.getElementsByClassName("db-load-more")[0].addEventListener("click", function(){
+
+                    var j = setInterval(function() {
+                        var list = document.getElementsByClassName("btn-standard btn-green-grad js-send-offer");
+                        //console.log(list.length);
+                        for (let item of list) {
+                            item.text = JSON.parse(item.getAttribute("data-meta"))['username'];;
+                        }
+                        console.log("FiverrBuyers-Done");
+                        if (list.length > 35) { clearInterval(j); }
+                        else { console.log("CarryON"); }
+                    },1000)
+
+
+                });
+
+                document.getElementsByClassName("js-search-submit btn-search")[0].addEventListener("click", function(){
 
                     var j = setInterval(function() {
                         var list = document.getElementsByClassName("btn-standard btn-green-grad js-send-offer");
